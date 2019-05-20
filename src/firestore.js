cda1b5204d5fa13887aa609c98d335799cc49e21
manitu85 <manitu.psy@gmail.com>
2019-05-20 20:04:00 +0200

    #

---
+++ src/firestore.js
@@ -0,0 +1,44 @@
+import { createStore, combineReducers, compose } from 'redux'
+import firebase from 'firebase'
+import 'firebase/firestore'
+import { firebaseConfig } from './firebase-config';
+import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase'
+import { reduxFirestore, firestoreReducer } from 'redux-firestore';
+// Reducers
+// @todos
+
+
+
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: 'users',
+  useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+}
+
+// Initialize firebase instance
+firebase.initializeApp(firebaseConfig)
+// Initialize firestore
+// const firestore = firebase.firestore() 
+
+
+// Add reactReduxFirebase enhancer when making store creator
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, rrfConfig), 
+  reduxFirestore(firebase) 
+)(createStore)
+
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer 
+})
+
+// Create store with reducers and initial state
+const initialState = {}
+const store = createStoreWithFirebase(rootReducer, initialState, compose(
+  reactReduxFirebase(firebase),
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&  window.__REDUX_DEVTOOLS_EXTENSION__()
+))
+
+export default store;
